5b537f77a624541b361aec383e80bd07a98ab4a2
fhmen <fh.menendez@outlook.com>
2016-05-16 00:59:48 -0600

    Spinner aniadidos a Activity Asignacion

--- app/src/main/java/work/tarea1/CrudActivities/AsignacionInsertarActivity.java
+++ app/src/main/java/work/tarea1/CrudActivities/AsignacionInsertarActivity.java
@@ -5,19 +5,25 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import work.tarea1.DataBaseHWork;
 import work.tarea1.PrivetClass.asignacion;
 import work.tarea1.R;
 
-public class AsignacionInsertarActivity extends AppCompatActivity {
+public class AsignacionInsertarActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private DataBaseHWork helper;
     private EditText idAsignacion;
-    private EditText idActividad;
-    private EditText idLocal;
+    private int idActividad;
+    private String idLocal;
+
+    Spinner spAsignacionInsert;
+    Spinner spinnerAsigancionLocal;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +32,24 @@ public class AsignacionInsertarActivity extends AppCompatActivity {
 
         helper = new DataBaseHWork(this);
         idAsignacion = (EditText) findViewById(R.id.idAsignacionLabel);
-        idActividad = (EditText) findViewById(R.id.idActividad);
-        idLocal = (EditText) findViewById(R.id.idLocal);
+//        idActividad = (EditText) findViewById(R.id.idActividad);
+//        idLocal = (EditText) findViewById(R.id.idLocal);
 
+
+        spAsignacionInsert=(Spinner)findViewById(R.id.spinnerAsignacionInsert);
+        spinnerAsigancionLocal=(Spinner)findViewById(R.id.spinnerAsigancionLocal);
+        spAsignacionInsert.setOnItemSelectedListener(this);
+        spinnerAsigancionLocal.setOnItemSelectedListener(this);
+
+        loadSpinnerData();
+
+
+
+    }
+
+    private void loadSpinnerData() {
+        spAsignacionInsert.setAdapter(helper.prepareSpinner(this,"Actividad", "descripcion", "id_actividad"));
+        spinnerAsigancionLocal.setAdapter(helper.prepareSpinner(this, "LOCAL", "ID_LOCAL", "ID_LOCAL"));
 
     }
 
@@ -39,24 +60,32 @@ public class AsignacionInsertarActivity extends AppCompatActivity {
         return true;
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+
+        Spinner spinner = (Spinner) parent;
+        if(spinner.getId() == R.id.spinnerAsignacionInsert)
+        {
+            String filter = parent.getItemAtPosition(position).toString();
+            idActividad= (Integer) helper.getValueSelectedSpineer("id_actividad", "descripcion", "Actividad",false,filter);
+            // Showing selected spinner item
+//            Toast.makeText(parent.getContext(), "You selected: " + idActividad,
+//                    Toast.LENGTH_LONG).show();
         }
-
-        return super.onOptionsItemSelected(item);
+        else if(spinner.getId() == R.id.spinnerAsigancionLocal)
+        {
+            String filter = parent.getItemAtPosition(position).toString();
+            idLocal= (String) helper.getValueSelectedSpineer("ID_LOCAL", "ID_LOCAL", "LOCAL",true,filter);
+           //Showing selected spinner item
+//            Toast.makeText(parent.getContext(), "You selected: " +  idLocal,
+//                    Toast.LENGTH_LONG).show();
+        }
     }
 
     public void insertarAsignacion(View view) {
         String regInsertados;
 
-        asignacion asignacion = new asignacion( idAsignacion.getText().toString(), idActividad.getText().toString(), idLocal.getText().toString());
+        asignacion asignacion = new asignacion( idAsignacion.getText().toString(), Integer.toString(idActividad) , idLocal.toString());
         helper.abrir();
         regInsertados = helper.insertarAsinacion(asignacion);
         helper.cerrar();
@@ -65,14 +94,16 @@ public class AsignacionInsertarActivity extends AppCompatActivity {
 
     public void limpiarTexto(View v) {
         idAsignacion.setText("");
-        idLocal.setText("");
-        idActividad.setText("");
+//        idLocal.setText("");
+//        idActividad.setText("");
 
     }
 
 
 
 
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
-
+    }
 }
